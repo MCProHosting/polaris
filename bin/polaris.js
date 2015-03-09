4f710d681f8dbceddf591b43d0ef4b908259671f
Connor Peet <connor@peet.io>
2015-03-08 23:00:04 -0400

    Some more basis stuff

---
+++ bin/polaris.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+var program = require('commander');
+var pack = require('../package');
+
+var Polaris = require('../lib/polaris');
+var serve = require('../lib/http/server');
+
+program
+    .version(pack.version)
+    .option('-s, --serve [host:port]', 'Runs the web interface and API on the address.')
+    .parse(process.argv);
+
+if (!program.args.length) {
+    program.help();
+}
+
+var polaris = new Polaris().boot();
+if (program.serve) {
+    serve(polaris, program.serve);
+}
+
